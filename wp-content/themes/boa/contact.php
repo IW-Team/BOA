3fc8d6a21e094393ec6d2a614f309f61e6329d3b
julien <julien.moratalla@gmail.com>
2017-02-09 19:16:55 +0100

    add contact form

--- wp-content/themes/boa/contact.php
+++ wp-content/themes/boa/contact.php
@@ -13,6 +13,21 @@ get_header();
 
 <h1>Contact</h1>
 
+<form class="contactForm" method="POST" action="#">
+    <p>Nom :</p>
+    <input type="text" name="lastname" class="_input">
+    <p>Prenom :</p>
+    <input type="text" name="firstname" class="_input">
+    <p>Email :</p>
+    <input type="text" name="email" class="_input">
+    <p>Message :</p>
+    <textarea rows="4" cols="50" name="content"></textarea><br>
+    <input class="_input" type="submit" value="Envoyer">
+</form>
+
+
+
+
 <?php
 get_footer();
 ?>
